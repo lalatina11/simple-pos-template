1e0d4d2034d8e8a02b9d2a58504b6539123c7a4b
lalatina11 <rahmadancandra@gmail.com>
2025-05-28 20:36:58 +0700

    fix: change redirect

--- src/pages/[[...index]].tsx
+++ src/pages/[[...index]].tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   useEffect(() => {
     if (isSignedIn && isLoaded) {
-      void router.replace("/dashboard");
+      void router.replace("/sign-in");
     }
   }, [isSignedIn, isLoaded, router]);
 
